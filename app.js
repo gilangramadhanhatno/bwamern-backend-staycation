7c00128f1c647b8e10666225bb688ffa0838d260
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2022-05-23 20:46:46 +0700

    debug cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+// const cors = require("cors");
 const cors = require("cors");
 // import mongoose
 const mongoose = require("mongoose");
@@ -44,6 +45,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/sb-admin-2", express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2")));
 
+// app.use(cors());
 app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
